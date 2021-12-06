2b6ff118ac06c0455d72b272d497ae5c4d8a3688
ARJUN-PRADEEP-123 <93504468+ARJUN-PRADEEP-123@users.noreply.github.com>
2021-12-06 21:19:14 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -45,7 +45,7 @@ async def start(bot, message):
                 ident, file_id = message.text.split("_-_-_-_")
                 await bot.send_message(
                     chat_id=message.from_user.id,
-                    url="https://telegra.ph/file/8005b3498755b538efa3c.jpg"
+                    url="https://telegra.ph/file/8005b3498755b538efa3c.jpg",
                     text="**PLEASE JOIN THE UPDATE CHANNEL BELOW TO USE ME!**",
                     reply_markup=InlineKeyboardMarkup(
                         [
